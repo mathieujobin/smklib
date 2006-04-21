8aa128559d3a287f34300675296858511af6759b
Mathieu Jobin <somekool@gmail.com>
2006-04-21 07:52:09 +0000

    smklib: fix error mailer
    git-svn-id: http://svn.somekool.net/nihonzuki/smklib@328 0aaf60d9-15f3-0310-80df-ebf7baf99ddd

--- lib/error_mailer.rb
+++ lib/error_mailer.rb
@@ -12,7 +12,7 @@ module ErrorMailerSupport
 	end
 
 	def error_mailer_from
-		'Error Mailer <error@somekool.net>'
+		'FRES:Error Mailer <error@somekool.net>'
 	end
 
 	def log_error(exception)
@@ -26,21 +26,17 @@ module ErrorMailerSupport
 		end
 	end
 
-end
-
-class ActionController::Base
 	def rescue_action_in_public(exception) #:doc:
-		#render_text exception.class.to_s
-		#return true
-		case exception.class
-		when ActionController::RoutingError, ActionController::UnknownAction
-			render_text(IO.read(File.join(RAILS_ROOT, 'public', 'smklib', '404.html')), "404 Not Found")
-		when Mysql::Error
-			render_text(IO.read(File.join(RAILS_ROOT, 'public', 'smklib', '500-mysql.html')), "500 Internal Server Error")
+		case exception.class.to_s
+		when "ActionController::RoutingError", "ActionController::UnknownAction"
+			render :file => "#{RAILS_ROOT}/public/smklib/404.html", :status => 404
+		when "Mysql::Error"
+			render :file => "#{RAILS_ROOT}/public/smklib/500-mysql.html", :status => 500
 		else
-			render_text(IO.read(File.join(RAILS_ROOT, 'public', 'smklib', '500.html')), "500 Internal Server Error")
+			render :file => "#{RAILS_ROOT}/public/smklib/500.html", :status => 500
 		end
 	end
+
 end
 
 class ErrorMailer < ActionMailer::Base
